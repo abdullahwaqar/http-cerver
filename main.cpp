edadb721d314accb86bde47e331e718f4a89aaa7
AWR417H <abdullah@aw.com>
2018-06-22 20:08:05 +0500

    Working Server

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
-#include "include/Socket.h"
+#include "include/Server.h"
 
 int main(int argc, char const *argv[]){
-    std::cout << "work";
+    std::stringstream ss(argv[1]);
+    int port;
+    ss >> port;
+    Server serv;
+    serv.bind("/", [] (http::Request) {
+        http::Response res;
+        std::stringstream message;
+        message << "<h1>Hey! Welcome to the site. <a href=\"info\">Click Me!</a></h1>";
+        res.write(message);
+        return res.generate();
+    });
+
+    serv.bind("/info", [] (http::Request) {
+        http::Response res;
+        std::stringstream message;
+        message << "<h1>Hello World, I'm a Http Server Written in C++.</h1>";
+        res.write(message);
+        return res.generate();
+    });
+
+    serv.listen(port);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-output: main.o Socket.o tcp_socket.o
-	g++ main.o Socket.o tcp_socket.o -o main && make clean
+output: main.o Socket.o tcp_socket.o Http.o Server.o
+	g++ main.o Socket.o tcp_socket.o Http.o Server.o -o main && make clean
 
 main.o: main.cpp
 	g++ -c  main.cpp
@@ -9,5 +9,11 @@ Socket.o: source/Socket.cc include/Socket.h
 
 tcp_socket.o: source/tcp_socket.cc include/tcp_socket.h
 	g++ -c  source/tcp_socket.cc
+
+Http.o: source/Http.cpp include/Http.h
+	g++ -c  source/Http.cpp
+
+Server.o: source/Server.cpp include/Server.h
+	g++ -c  source/Server.cpp
 clean:
 	rm *.o
